983fb83170b7bbaf3a059c422a3181a855266c06
Thomas Andoerson <t11omas1983@live.co.uk>
2026-09-03 11:46:25 +0100

    reorganised folder structure

---
+++ tests/TradeImportsQuantityMgmt.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TradeImportsQuantityMgmt.IntegrationTests
+{
+    [Trait("Category", "IntegrationTest")]
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
